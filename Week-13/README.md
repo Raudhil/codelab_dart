6c8f62c0c55e2669d78c031ac9951f8e7cf4b5ce
raul <raudhilfirdausn@gmail.com>
2025-11-24 10:50:13 +0700

    W13: Jawaban Soal 3

--- Week-13/README.md
+++ Week-13/README.md
@@ -16,12 +16,19 @@
 
 ![alt text](<img/image copy.png>)
 
-Kode-kode tersebut bertujuan untuk memuat data pizza dari file JSON lokal, menyimpannya sebagai String dalam state aplikasi, dan menampilkannya langsung (masih dalam format JSON mentah) di layar aplikasi Flutter setelah widget utama (_MyHomePageState) diinisialisasi.
+Kode-kode pada langkah 1-10 bertujuan untuk memuat data pizza dari file JSON lokal, menyimpannya sebagai String dalam state aplikasi, dan menampilkannya langsung (masih dalam format JSON mentah) di layar aplikasi Flutter setelah widget utama (_MyHomePageState) diinisialisasi.
 
 **Soal 2**
 
 ![alt text](<img/image copy.png>)
 
+**Hasil**
 
+![alt text](<img/image copy 2.png>)
 
+Kode-kode pada langkah 11-21 berfungsi untuk memuat data JSON mentah, mengonversinya menjadi objek Dart terstruktur (List<Pizza>), dan kemudian menggunakan daftar objek tersebut untuk membangun Widget UI (kemungkinan ListView atau Column) yang menampilkan pizzaName sebagai judul tebal dan description sebagai teks pendukung untuk setiap item, sehingga tampilannya rapi dan terorganisir, bukan lagi sebagai string JSON mentah.
+
+**Soal 3**
+
+![alt text](<img/image copy 2.png>)
 
--- Week-13/store_data_raul/lib/main.dart
+++ Week-13/store_data_raul/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'model/pizza.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,27 +29,54 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String pizzaString = '';
+  List<Pizza> myPizzas = [];
+  // Future readJsonFile() async {
+  //   String myString = await DefaultAssetBundle.of(
+  //     context,
+  //   ).loadString('lib/assets/pizzalists.json');
+  //   setState(() {
+  //     pizzaString = myString;
+  //   });
+  // }
 
-  Future readJsonFile() async {
+  Future<List<Pizza>> readJsonFile() async {
     String myString = await DefaultAssetBundle.of(
       context,
     ).loadString('lib/assets/pizzalists.json');
-    setState(() {
-      pizzaString = myString;
-    });
+    List pizzaMapList = jsonDecode(myString);
+
+    List<Pizza> myPizzas = [];
+    for (var pizza in pizzaMapList) {
+      Pizza myPizza = Pizza.fromJson(pizza);
+      myPizzas.add(myPizza);
+    }
+
+    return myPizzas;
   }
 
   @override
   void initState() {
     super.initState();
-    readJsonFile();
+    readJsonFile().then((value) {
+      setState(() {
+        myPizzas = value;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('JSON')),
-      body: Text(pizzaString),
+      body: ListView.builder(
+        itemCount: myPizzas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(myPizzas[index].pizzaName),
+            subtitle: Text(myPizzas[index].description),
+          );
+        },
+      ),
     );
   }
 }
